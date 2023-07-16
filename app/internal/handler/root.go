ebf06eb2f058a014ebb7142bb1dc3e074247a67c
Yuki Higuchi <yuki@yhiguchi.dev>
2023-07-16 23:27:17 +0900

    chore: Add cors header

--- app/internal/handler/root.go
+++ app/internal/handler/root.go
@@ -37,6 +37,9 @@ type DeleteHandler interface {
 }
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	switch r.Method {
 	case http.MethodGet:
 		handleGet(w, r)
